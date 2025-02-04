b5e3a5d15d00d4587d07ac34c391dff8a1321d6c
StacyAceIt <30737898+StacyAceIt@users.noreply.github.com>
2025-02-04 10:08:47 -0500

    retyped script.js to learn what it does

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const leftContainer = document.querySelector('.left_container');
+const leftContainer = document.querySelector(".left_container");
 const image = new Image();
-image.src = './image.avif'; // Set the same image as the background
+image.src = "./image.avif";
 
-image.onload = function() {
+image.onload = function(){
   const aspectRatio = image.width / image.height;
-  leftContainer.style.width = `${window.innerHeight * aspectRatio}px`; // Adjust width dynamically
-};
+  leftContainer.style.width = `${window.innerHeight * aspectRatio}px`;
+}
 
-// Update width on window resize
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   if (image.complete) {
     leftContainer.style.width = `${window.innerHeight * (image.width / image.height)}px`;
   }
-});
+})
